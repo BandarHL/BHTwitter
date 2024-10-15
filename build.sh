0a217a2961c0cced859790b159fd8c2302b6a95c
zx <zxcvbn@hrzn.email>
2024-10-14 21:21:37 -0400

    chore: azule -> cyan for injection

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 LONG=sideloaded:,rootless:,trollstore
 OPTS=$(getopt -a weather --longoptions "$LONG" -- "$@")
-libcephei_URL="https://web.archive.org/web/20240222081324/https://cdn.discordapp.com/attachments/755439561454256132/1184388888475738243/libcephei.zip"
-PROJECT_PATH=$PWD
 
 while :; do
   case "$1" in
@@ -22,16 +20,10 @@ while :; do
       fi
 
       if [ -e ./packages/com.atebits.Tweetie2.ipa ]; then
-
-        echo -e '\033[1m\033[32mDownloading libcephei SDK.\033[0m'
-        temp_dir=$(mktemp -d)
-        curl -L -o "$temp_dir/libcephei.zip" "$libcephei_URL"
-        unzip -o "$temp_dir/libcephei.zip" -d ./packages
-        rm -rf "$temp_dir"
-        rm -rf ./packages/__MACOSX
-
         echo -e '\033[1m\033[32mBuilding the IPA.\033[0m'
-        azule -i "$PROJECT_PATH/packages/com.atebits.Tweetie2.ipa" -o "$PROJECT_PATH/packages" -n BHTwitter-sideloaded -r -f "$PROJECT_PATH/.theos/obj/debug/keychainfix.dylib" "$PROJECT_PATH/.theos/obj/debug/libbhFLEX.dylib" "$PROJECT_PATH/.theos/obj/debug/BHTwitter.dylib" "$PROJECT_PATH/packages/Cephei.framework" "$PROJECT_PATH/packages/CepheiUI.framework" "$PROJECT_PATH/packages/CepheiPrefs.framework" "$PROJECT_PATH/layout/Library/Application Support/BHT/BHTwitter.bundle"
+        cyan -i packages/com.atebits.Tweetie2.ipa -o packages/BHTwitter-sideloaded --ignore-encrypted \
+          -uwf .theos/obj/debug/keychainfix.dylib .theos/obj/debug/libbhFLEX.dylib \
+          .theos/obj/debug/BHTwitter.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
 
         echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
       else
@@ -65,18 +57,10 @@ while :; do
       fi
 
       if [ -e ./packages/com.atebits.Tweetie2.ipa ]; then
-
-        echo -e '\033[1m\033[32mDownloading libcephei SDK.\033[0m'
-        temp_dir=$(mktemp -d)
-        curl -L -o "$temp_dir/libcephei.zip" "$libcephei_URL"
-        unzip -o "$temp_dir/libcephei.zip" -d ./packages
-        rm -rf "$temp_dir"
-        rm -rf ./packages/__MACOSX
-
         echo -e '\033[1m\033[32mBuilding the IPA.\033[0m'
 
-        azule -i "$PROJECT_PATH/packages/com.atebits.Tweetie2.ipa" -o "$PROJECT_PATH/packages" -n BHTwitter-trollstore -r -f "$PROJECT_PATH/.theos/obj/debug/BHTwitter.dylib" "$PROJECT_PATH/.theos/obj/debug/libbhFLEX.dylib" "$PROJECT_PATH/packages/Cephei.framework" "$PROJECT_PATH/packages/CepheiUI.framework" "$PROJECT_PATH/packages/CepheiPrefs.framework" "$PROJECT_PATH/layout/Library/Application Support/BHT/BHTwitter.bundle"
-        mv "$PROJECT_PATH/packages/BHTwitter-trollstore.ipa" "$PROJECT_PATH/packages/BHTwitter-trollstore.tipa"
+        cyan -i packages/com.atebits.Tweetie2.ipa -o packages/BHTwitter-trollstore.tipa --ignore-encrypted \
+          -uwf .theos/obj/debug/BHTwitter.dylib .theos/obj/debug/libbhFLEX.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
 
         echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
       else
