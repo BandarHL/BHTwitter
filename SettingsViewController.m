1ba0b19582ae9dd3b11f6db257a5c94bc4f7fc3a
aridan <adnanbukvic862@gmail.com>
2025-05-27 23:09:28 +0200

    Update SettingsViewController.m

--- SettingsViewController.m
+++ SettingsViewController.m
@@ -104,52 +104,41 @@ static UIFont *TwitterChirpFont(TwitterFontStyle style) {
 
 
 - (void)viewDidLoad {
-
     if (self.twAccount != nil) {
         self.navigationItem.titleView = [objc_getClass("TFNTitleView") titleViewWithTitle:[[BHTBundle sharedBundle]
-                               localizedStringForKey:@"BHTWITTER_SETTINGS_TITLE"] subtitle:self.twAccount.displayUsername];
+            localizedStringForKey:@"BHTWITTER_SETTINGS_TITLE"] subtitle:self.twAccount.displayUsername];
     } else {
-        self.title = [[BHTBundle sharedBundle]
-                               localizedStringForKey:@"BHTWITTER_SETTINGS_TITLE"];
+        self.title = [[BHTBundle sharedBundle] localizedStringForKey:@"BHTWITTER_SETTINGS_TITLE"];
     }
 
     [super viewDidLoad];
 
-    UILabel *subtitleLabel = [[UILabel alloc] init];
-    subtitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    UILabel *subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 40)];
     subtitleLabel.font = [TwitterChirpFont(TwitterFontStyleRegular) fontWithSize:13];
     subtitleLabel.textColor = [UIColor secondaryLabelColor];
     subtitleLabel.numberOfLines = 0;
     subtitleLabel.textAlignment = NSTextAlignmentLeft;
     subtitleLabel.text = [[BHTBundle sharedBundle] localizedStringForKey:@"APP_ICON_HEADER_TITLE"];
-    [self.view addSubview:subtitleLabel];
 
-    [NSLayoutConstraint activateConstraints:@[
-        [subtitleLabel.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:16],
-        [subtitleLabel.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-16],
-        [subtitleLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:8],
-        [self.table.topAnchor constraintEqualToAnchor:subtitleLabel.bottomAnchor constant:8]
-    ]];
+    UIView *headerContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 52)];
+    subtitleLabel.frame = CGRectInset(headerContainer.bounds, 16, 6);
+    [headerContainer addSubview:subtitleLabel];
+    self.table.tableHeaderView = headerContainer;
 
-    // Set the background color to match system background
     self.view.backgroundColor = [UIColor systemBackgroundColor];
-    
-    // Configure the table view to blend with background
     self.table.backgroundColor = [UIColor systemBackgroundColor];
     self.table.separatorColor = [UIColor separatorColor];
-    
-    // Remove extra separators below content
     self.table.tableFooterView = [UIView new];
     self.table.separatorStyle = UITableViewCellSeparatorStyleNone;
 
     if (@available(iOS 15.0, *)) {
-        self.table.sectionHeaderTopPadding = 8; 
+        self.table.sectionHeaderTopPadding = 8;
     }
-    
-    // These ensure cells align with headers
+
     self.table.separatorInset = UIEdgeInsetsMake(0, 16, 0, 0);
     self.table.layoutMargins = UIEdgeInsetsMake(0, 16, 0, 16);
 }
+
 
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
