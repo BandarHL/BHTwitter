db787a0d955debf64d0b42e9b6915c04119be021
aridan <adnanbukvic862@gmail.com>
2025-05-30 06:23:07 +0200

    Update BHCustomTabBarViewController.m

--- CustomTabBar/BHCustomTabBarViewController.m
+++ CustomTabBar/BHCustomTabBarViewController.m
@@ -3,38 +3,18 @@
 //  NeoFeeBird
 //
 //  Created by Bandar Alruwaili on 11/12/2023.
-//  Modified by actuallyaridan on 28/04/2025.
+//  Modified by actuallyaridan on 30/05/2025.
 //
 
 #import "BHCustomTabBarViewController.h"
 #import "BHCustomTabBarUtility.h"
 #import "../BHTBundle/BHTBundle.h"
-#import "Colours/Colours.h" 
+#import "Colours/Colours.h"
 
-typedef NS_ENUM(NSInteger, TwitterFontStyle) {
-    TwitterFontStyleRegular,
-    TwitterFontStyleSemibold,
-    TwitterFontStyleBold
-};
-
-static UIFont *TwitterChirpFont(TwitterFontStyle style) {
-    switch (style) {
-        case TwitterFontStyleBold:
-            return [UIFont fontWithName:@"ChirpUIVF_wght3200000_opsz150000" size:17] ?: 
-                   [UIFont systemFontOfSize:17 weight:UIFontWeightBold];
-        case TwitterFontStyleSemibold:
-            return [UIFont fontWithName:@"ChirpUIVF_wght2BC0000_opszE0000" size:14] ?: 
-                   [UIFont systemFontOfSize:14 weight:UIFontWeightSemibold];
-        case TwitterFontStyleRegular:
-        default:
-            return [UIFont fontWithName:@"ChirpUIVF_wght1900000_opszE0000" size:12] ?: 
-                   [UIFont systemFontOfSize:12 weight:UIFontWeightRegular];
-    }
-}
-
-@interface BHCustomTabBarViewController () <UITableViewDelegate, UITableViewDataSource>
-@property (nonatomic, strong) UITableView *tableView;
-@property (nonatomic, strong) NSMutableArray <BHCustomTabBarSection *> *data;
+@interface BHCustomTabBarViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) NSMutableArray<BHCustomTabBarItem *> *allItems;
+@property (nonatomic, strong) NSMutableSet<NSString *> *enabledPageIDs;
 @end
 
 @implementation BHCustomTabBarViewController
@@ -43,60 +23,99 @@ static UIFont *TwitterChirpFont(TwitterFontStyle style) {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor systemBackgroundColor];
 
-    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleInsetGrouped];
-    self.tableView.delegate = self;
-    self.tableView.dataSource = self;
-    self.tableView.dragInteractionEnabled = YES;
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
-    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
-    self.tableView.editing = YES;
+    // Save button in nav bar
+    UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithTitle:@"Save"
+                                                                    style:UIBarButtonItemStyleDone
+                                                                   target:self
+                                                                   action:@selector(saveState)];
+    self.navigationItem.rightBarButtonItem = saveButton;
 
-    // Style
-    self.tableView.backgroundColor = [UIColor systemBackgroundColor];
-    self.tableView.separatorColor = [UIColor separatorColor];
-    self.tableView.separatorInset = UIEdgeInsetsMake(0, 16, 0, 0);
-    self.tableView.layoutMargins = UIEdgeInsetsMake(0, 16, 0, 16);
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    // Layout
+CGFloat padding = 20 * 2 + 20 * 2; // section inset + 2 gaps
+CGFloat itemWidth = (self.view.bounds.size.width - padding) / 3;
 
-    [self.view addSubview:self.tableView];
+UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+layout.itemSize = CGSizeMake(itemWidth, itemWidth + 30); // 30 for label height
+layout.minimumInteritemSpacing = 20;
+layout.minimumLineSpacing = 20;
+layout.sectionInset = UIEdgeInsetsMake(20, 20, 20, 20);
 
-    [NSLayoutConstraint activateConstraints:@[
-        [self.tableView.topAnchor constraintEqualToAnchor:self.view.topAnchor],
-        [self.tableView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
-        [self.tableView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
-        [self.tableView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor]
-    ]];
+    // Collection view setup
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
+    self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.collectionView.backgroundColor = [UIColor systemBackgroundColor];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"tabItemCell"];
+    [self.view addSubview:self.collectionView];
 
-    self.data = [NSMutableArray new];
-    [self getData];
-    [self.tableView reloadData];
-    [self resetSettingsBarButton];
+    // Restore button
+UIButton *restoreButton = [UIButton buttonWithType:UIButtonTypeSystem];
+[restoreButton setTitle:@"Restore to default" forState:UIControlStateNormal];
+restoreButton.titleLabel.font = [UIFont systemFontOfSize:16 weight:UIFontWeightSemibold];
+[restoreButton setTitleColor:[UIColor labelColor] forState:UIControlStateNormal];
+restoreButton.backgroundColor = [UIColor systemGray6Color];
+restoreButton.layer.cornerRadius = 26;
+restoreButton.contentEdgeInsets = UIEdgeInsetsMake(12, 0, 12, 0);
+restoreButton.translatesAutoresizingMaskIntoConstraints = NO;
+[restoreButton addTarget:self action:@selector(resetSettingsBarButtonHandler:) forControlEvents:UIControlEventTouchUpInside];
+[self.view addSubview:restoreButton];
+
+    // Layout constraints
+[NSLayoutConstraint activateConstraints:@[
+    [restoreButton.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:-20],
+    [restoreButton.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+    [restoreButton.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20],
+    [restoreButton.heightAnchor constraintEqualToConstant:52],
+
+    [self.collectionView.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+    [self.collectionView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
+    [self.collectionView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
+    [self.collectionView.bottomAnchor constraintEqualToAnchor:restoreButton.topAnchor constant:-10]
+]];
+    [self loadData];
 }
 
-- (void)resetSettingsBarButton {
-    UIBarButtonItem *resetButton = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"trash"] style:UIBarButtonItemStylePlain target:self action:@selector(resetSettingsBarButtonHandler:)];
-    self.navigationItem.rightBarButtonItem = resetButton;
+#pragma mark - Data
+
+- (UIColor *)disabledBorderColorForCurrentMode {
+    if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark) {
+        return [UIColor systemGray6Color];
+    } else {
+        return [UIColor whiteColor];
+    }
 }
 
-- (void)resetSettingsBarButtonHandler:(UIBarButtonItem *)sender {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"BHTwitter"
-                                                                   message:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_REST_MESSAGE"]
-                                                            preferredStyle:UIAlertControllerStyleAlert];
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+    [super traitCollectionDidChange:previousTraitCollection];
+    [self.collectionView reloadData];
+}
 
-    [alert addAction:[UIAlertAction actionWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"YES_BUTTON_TITLE"]
-                                              style:UIAlertActionStyleDefault
-                                            handler:^(UIAlertAction * _Nonnull action) {
-        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"allowed"];
-        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"hidden"];
-        [self getData];
-        [self.tableView reloadData];
-    }]];
+- (void)loadData {
+    NSArray<BHCustomTabBarItem *> *savedAllowed = [self getItemsForKey:@"allowed"];
+    NSArray<BHCustomTabBarItem *> *savedHidden = [self getItemsForKey:@"hidden"];
 
-    [alert addAction:[UIAlertAction actionWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"NO_BUTTON_TITLE"]
-                                              style:UIAlertActionStyleCancel
-                                            handler:nil]];
+    if (savedAllowed && savedHidden) {
+        self.allItems = [[savedAllowed arrayByAddingObjectsFromArray:savedHidden] mutableCopy];
+        self.enabledPageIDs = [NSMutableSet set];
+        for (BHCustomTabBarItem *item in savedAllowed) {
+            [self.enabledPageIDs addObject:item.pageID];
+        }
+    } else {
+        self.allItems = [@[
+            [[BHCustomTabBarItem alloc] initWithTitle:@"Home" pageID:@"home"],
+            [[BHCustomTabBarItem alloc] initWithTitle:@"Guide" pageID:@"guide"],
+            [[BHCustomTabBarItem alloc] initWithTitle:@"Spaces" pageID:@"audiospace"],
+            [[BHCustomTabBarItem alloc] initWithTitle:@"Communities" pageID:@"communities"],
+            [[BHCustomTabBarItem alloc] initWithTitle:@"NTAB" pageID:@"ntab"],
+            [[BHCustomTabBarItem alloc] initWithTitle:@"Messages" pageID:@"messages"],
+            [[BHCustomTabBarItem alloc] initWithTitle:@"Grok" pageID:@"grok"],
+            [[BHCustomTabBarItem alloc] initWithTitle:@"Media" pageID:@"media"]
+        ] mutableCopy];
+        self.enabledPageIDs = [NSMutableSet setWithArray:@[@"home", @"guide", @"audiospace", @"communities"]];
+    }
 
-    [self presentViewController:alert animated:YES completion:nil];
+    [self.collectionView reloadData];
 }
 
 - (NSArray<BHCustomTabBarItem *> *)getItemsForKey:(NSString *)key {
@@ -107,78 +126,110 @@ static UIFont *TwitterChirpFont(TwitterFontStyle style) {
     return nil;
 }
 
-- (void)getData {
-    NSArray<BHCustomTabBarItem *> *savedAllowedArr = [self getItemsForKey:@"allowed"];
-    NSArray<BHCustomTabBarItem *> *savedHiddenArr = [self getItemsForKey:@"hidden"];
+- (void)saveState {
+    NSMutableArray *enabledItems = [NSMutableArray array];
+    NSMutableArray *disabledItems = [NSMutableArray array];
 
-    if (savedAllowedArr && savedHiddenArr) {
-        self.data = [@[
-            [[BHCustomTabBarSection alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_SECTION_1_TITLE"] items:savedAllowedArr],
-            [[BHCustomTabBarSection alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_SECTION_2_TITLE"] items:savedHiddenArr]
-        ] mutableCopy];
-    } else {
-        self.data = [@[
-            [[BHCustomTabBarSection alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_SECTION_1_TITLE"] items:@[
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_1"] pageID:@"home"],
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_2"] pageID:@"guide"],
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_3"] pageID:@"audiospace"],
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_4"] pageID:@"communities"],
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_5"] pageID:@"ntab"],
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_6"] pageID:@"messages"],
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_7"] pageID:@"grok"],
-                [[BHCustomTabBarItem alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_OPTION_8"] pageID:@"media"]
-            ]],
-            [[BHCustomTabBarSection alloc] initWithTitle:[[BHTBundle sharedBundle] localizedStringForKey:@"CUSTOM_TAB_BAR_SECTION_2_TITLE"] items:@[]]
-        ] mutableCopy];
+    for (BHCustomTabBarItem *item in self.allItems) {
+        if ([self.enabledPageIDs containsObject:item.pageID]) {
+            [enabledItems addObject:item];
+        } else {
+            [disabledItems addObject:item];
+        }
     }
+
+    NSData *enabledData = [NSKeyedArchiver archivedDataWithRootObject:enabledItems];
+    NSData *disabledData = [NSKeyedArchiver archivedDataWithRootObject:disabledItems];
+
+    [[NSUserDefaults standardUserDefaults] setObject:enabledData forKey:@"allowed"];
+    [[NSUserDefaults standardUserDefaults] setObject:disabledData forKey:@"hidden"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
-- (void)updateData {
-    [self.data[0] saveItemsForKey:@"allowed"];
-    [self.data[1] saveItemsForKey:@"hidden"];
+- (void)resetSettingsBarButtonHandler:(UIBarButtonItem *)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"BHTwitter"
+                                                                   message:@"Reset tab bar layout to default?"
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"allowed"];
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"hidden"];
+        [self loadData];
+        [self saveState];
+    }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleCancel handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return self.data.count;
+#pragma mark - UICollectionView
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.data[section].items.count;
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.allItems.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    cell.textLabel.text = self.data[indexPath.section].items[indexPath.row].title;
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"tabItemCell" forIndexPath:indexPath];
 
-    // Style each cell properly
-    cell.backgroundColor = [UIColor systemBackgroundColor];
-    cell.textLabel.font = TwitterChirpFont(TwitterFontStyleSemibold);
-    cell.textLabel.textColor = [UIColor labelColor];
+    for (UIView *view in cell.contentView.subviews) {
+        [view removeFromSuperview];
+    }
+
+    BHCustomTabBarItem *item = self.allItems[indexPath.item];
+    BOOL isEnabled = [self.enabledPageIDs containsObject:item.pageID];
+
+    // Border colors
+    UIColor *borderColor = isEnabled ? [UIColor systemBlueColor] : [self disabledBorderColorForCurrentMode];
+    UIColor *bgColor = [UIColor systemBackgroundColor];
+
+    CGFloat boxSize = cell.contentView.bounds.size.width;
+
+    // Container
+    UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, boxSize, boxSize)];
+    container.layer.cornerRadius = 12;
+    container.layer.borderWidth = 2;
+    container.layer.borderColor = [borderColor resolvedColorWithTraitCollection:self.traitCollection].CGColor;
+    container.backgroundColor = bgColor;
+
+    // Subtle shadow
+container.layer.shadowColor = [UIColor blackColor].CGColor;
+container.layer.shadowOpacity = 0.08;  // Increased from 0.05
+container.layer.shadowOffset = CGSizeMake(0, 4);  // Slightly larger vertical offset
+container.layer.shadowRadius = 10;  // Larger blur radius
+container.layer.masksToBounds = NO;
+
+    [cell.contentView addSubview:container];
+
+    // Icon (placeholder for now)
+    UIView *icon = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 28, 28)];
+    icon.backgroundColor = [UIColor systemGray4Color];
+    icon.layer.cornerRadius = 6;
+    icon.center = CGPointMake(container.bounds.size.width / 2, container.bounds.size.height / 2);
+    [container addSubview:icon];
+
+    // Label below container
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(container.frame) + 2, boxSize, 20)];
+    label.text = item.title;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontOfSize:13 weight:UIFontWeightRegular];
+    label.textColor = [UIColor labelColor];
+    [cell.contentView addSubview:label];
 
     return cell;
 }
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    BHCustomTabBarItem *item = self.allItems[indexPath.item];
 
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return self.data[section].title;
-}
-
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return (indexPath.section == 0) ? UITableViewCellEditingStyleDelete : UITableViewCellEditingStyleInsert;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        BHCustomTabBarItem *item = self.data[indexPath.section].items[indexPath.row];
-        [self.data[1].items addObject:item];
-        [self.data[indexPath.section].items removeObjectAtIndex:indexPath.row];
-        [tableView moveRowAtIndexPath:indexPath toIndexPath:[NSIndexPath indexPathForRow:(self.data[1].items.count - 1) inSection:1]];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        BHCustomTabBarItem *item = self.data[indexPath.section].items[indexPath.row];
-        [self.data[0].items addObject:item];
-        [self.data[indexPath.section].items removeObjectAtIndex:indexPath.row];
-        [tableView moveRowAtIndexPath:indexPath toIndexPath:[NSIndexPath indexPathForRow:(self.data[0].items.count - 1) inSection:0]];
+    if ([self.enabledPageIDs containsObject:item.pageID]) {
+        [self.enabledPageIDs removeObject:item.pageID];
+    } else {
+        [self.enabledPageIDs addObject:item.pageID];
     }
-    [self updateData];
+
+    [self saveState];
+    [collectionView reloadItemsAtIndexPaths:@[indexPath]];
 }
 
 @end
