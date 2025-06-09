0c65fbe8b55b436ae8328d16bdf24dc2bfe6c0a9
Thea <nyaathea@outlook.com>
2025-06-08 19:01:59 -0500

    Update: 2025-06-09 00:01:56 UTC

--- ModernSettingsViewController.m
+++ ModernSettingsViewController.m
@@ -4,8 +4,8 @@
 
 @interface ModernSettingsViewController ()
 @property (nonatomic, strong) TFNTwitterAccount *account;
-@property (nonatomic, strong) NSArray *sections;
-@property (nonatomic, strong) id descriptionAdapter;
+// @property (nonatomic, strong) NSArray *sections;
+// @property (nonatomic, strong) id descriptionAdapter;
 @end
 
 @implementation ModernSettingsViewController
@@ -15,7 +15,7 @@
     if (self) {
         NSLog(@"[BHTwitter] Initializing ModernSettingsViewController as a UITableViewController subclass.");
         self.account = account;
-        self.descriptionAdapter = [[objc_getClass("TFNSettingsDescriptionItemTableRowAdapter") alloc] init];
+        // self.descriptionAdapter = [[objc_getClass("TFNSettingsDescriptionItemTableRowAdapter") alloc] init];
     }
     return self;
 }
@@ -23,29 +23,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Modern BHTwitter";
-
-    id descriptionItem = [[objc_getClass("TFNSettingsDescriptionItem") alloc] initWithText:@"Welcome to the new BHTwitter settings! This is a work in progress." callsToAction:nil];
-    id section = @[descriptionItem];
-    self.sections = @[section];
+    self.view.backgroundColor = [UIColor blueColor]; 
+    self.tableView.backgroundColor = [UIColor greenColor];
+    // id descriptionItem = [[objc_getClass("TFNSettingsDescriptionItem") alloc] initWithText:@"Welcome to the new BHTwitter settings! This is a work in progress." callsToAction:nil];
+    // id section = @[descriptionItem];
+    // self.sections = @[section];
 }
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return self.sections.count;
+    return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.sections[section] count];
+    return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    id item = self.sections[indexPath.section][indexPath.row];
+    // id item = self.sections[indexPath.section][indexPath.row];
     
     // For now, we only have one type of item. We will expand this.
-    if ([item isKindOfClass:objc_getClass("TFNSettingsDescriptionItem")]) {
-        return [self.descriptionAdapter dataViewController:self tableViewCellForItem:item withOptions:nil atIndexPath:indexPath];
-    }
+    // if ([item isKindOfClass:objc_getClass("TFNSettingsDescriptionItem")]) {
+    //     return [self.descriptionAdapter dataViewController:self tableViewCellForItem:item withOptions:nil atIndexPath:indexPath];
+    // }
     
     return [[UITableViewCell alloc] init];
 }
